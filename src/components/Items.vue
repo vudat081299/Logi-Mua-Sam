680343595a2340e5f1bb3b1d658f47c20cb15fb1
Trung Nguyễn <trungpltb@gmail.com>
2021-03-31 12:07:13 +0700

    update

--- src/components/Items.vue
+++ src/components/Items.vue
@@ -1,153 +1,262 @@
 <template>
-    <v-card
-      class="d-flex fixedDivContent mx-auto flex-wrap pa-0 pb-9 align-center"
-      color="#ffffff"
-      flat
-      tile
-    >
-      <template
-        v-for="n in 101"
-      >
-        <v-hover
-          v-slot="{ hover }"
-          :key="n"
+  <v-card
+    class="d-flex fixedDivContent mx-auto flex-wrap pa-0 pb-9 align-center"
+    color="#ffffff"
+    flat
+    tile
+  >
+    <template v-for="n in 101">
+      <v-hover v-slot="{ hover }" :key="n">
+        <v-card
+          :class="{ 'on-hover': hover }"
+          class="hover-grow ma-4 hover-card elevation-1"
+          tile
+          color="#ffffff00"
+          width="300px"
+          height="350px"
         >
-          <v-card
-            :class="{ 'on-hover': hover }"
-            class="hover-grow ma-4 hover-card elevation-1"
-            tile
-            color="#ffffff00"
-            width="300px"
-            height="350px"
-            href="/detailitem"
-          >
-            <!-- :elevation="hover ? 2 : 0" -->
-            <!-- :style="`border-top: ${hover ? 4 : 0}px solid #42A5F5;border-right: ${hover ? 4 : 0}px solid #42A5F5;border-bottom: ${hover ? 4 : 0}px solid #42A5F5;border-left: ${hover ? 4 : 0}px solid #42A5F5;`" -->
-            <!-- <v-img
+          <!-- :elevation="hover ? 2 : 0" -->
+          <!-- :style="`border-top: ${hover ? 4 : 0}px solid #42A5F5;border-right: ${hover ? 4 : 0}px solid #42A5F5;border-bottom: ${hover ? 4 : 0}px solid #42A5F5;border-left: ${hover ? 4 : 0}px solid #42A5F5;`" -->
+          <!-- <v-img
               :src="`https://picsum.photos/200/300?image=${getImage()}`"
               height="175px"
             > -->
-            <v-img
-              :class="{ 'on-hover': hover }"
-              :src="`https://picsum.photos/500/300?image=${n * 5 + 10}`"
-              height="200px"
-              class="ma-4 mb-2 rounded-t-sm hover-img"
-            >
-              <!-- src="@/assets/e8d149d839d14346d2c14731f7f84f30.jpg" -->
-              <!-- src="https://i.picsum.photos/id/866/200/300.jpg?hmac=rcadCENKh4rD6MAp6V_ma-AyWv641M4iiOpe1RyFHeI" -->
+          <v-img
+            :class="{ 'on-hover': hover }"
+            :src="`https://picsum.photos/500/300?image=${n * 5 + 10}`"
+            height="200px"
+            class="ma-4 mb-2 rounded-t-sm hover-img"
+            @click="detailItem()"
+          >
+            <!-- src="@/assets/e8d149d839d14346d2c14731f7f84f30.jpg" -->
+            <!-- src="https://i.picsum.photos/id/866/200/300.jpg?hmac=rcadCENKh4rD6MAp6V_ma-AyWv641M4iiOpe1RyFHeI" -->
 
-              <!-- <span
+            <!-- <span
                 class="headline white--text pl-4 pt-4 d-inline-block"
                 v-text="cardState"
               >
               </span> -->
-            </v-img>
-            <v-card-text class="px-4 py-0">
-              <p class="black--text cut-text">
-                <span style="background: rgb(242, 242, 242); line-height: 12px; display: inline-block; padding: 0px 4px; font-size: 9px; margin: 0px 4px 0px 0px; position: relative; top: -1px; border-radius: 2px;">Ad</span>
-                  Máy nhào bột/thực phẩm đa năng DK30: Dùng để nhào, trộn mọi loại thực phẩm. Hàng chính hãng Thailand. Dùng cho hộ gia đình, hộ kinh doanh, doanh nghiệp.
-              </p>
-              <v-row
-                class="ma-0 px-0 align-center"
-              >
-                <v-rating
-                  v-model="rating[n]"
-                  :value="3"
-                  color="amber"
-                  background-color="#dddddd"
-                  empty-icon="mdi-star"
-                  dense
-                  size="13"
-                ></v-rating>
+          </v-img>
+          <v-card-text class="px-4 py-0">
+            <a class="black--text cut-text" href="/detailitem">
+              <span
+                style="
+                  background: rgb(242, 242, 242);
+                  line-height: 12px;
+                  display: inline-block;
+                  padding: 0px 4px;
+                  font-size: 9px;
+                  margin: 0px 4px 0px 0px;
+                  position: relative;
+                  top: -1px;
+                  border-radius: 2px;
+                "
+                >Ad</span>
+              Máy nhào bột/thực phẩm đa năng DK30: Dùng để nhào, trộn mọi loại
+              thực phẩm. Hàng chính hãng Thailand. Dùng cho hộ gia đình, hộ kinh
+              doanh, doanh nghiệp.
+            </a>
 
-                <div class="caption ml-1">
-                  (4000 lượt đánh giá)
-                </div>
-              </v-row>
+            <v-row class="ma-0 px-0 align-center">
+              <v-rating
+                v-model="rating[n]"
+                :value="3"
+                color="amber"
+                background-color="#dddddd"
+                empty-icon="mdi-star"
+                dense
+                size="13"
+              ></v-rating>
 
-              <v-row
-                class="ma-0 pa-0 align-center"
+              <div class="caption ml-1">(4000 lượt đánh giá)</div>
+            </v-row>
+            <v-row class="ma-0 pa-0 align-center">
+              <div
+                class="black--text text-decoration-line-through mb-n2"
+                style="font-size: 11px; text-align: right"
               >
-                <div class="black--text text-decoration-line-through mb-n2" style="font-size: 11px; text-align: right;">
-                  1.000.000
-                  <span class="text-decoration-underline">đ</span>
-                </div>
-              </v-row>
-              <v-row
-                class="ma-0 px-0 align-center"
-              >
-                <div class="text-subtitle-2 black--text">
-                  1.000.000
-                  <span class="text-decoration-underline">đ</span>
-                  <span class="" style="color: white; background: rgb(255, 66, 78); line-height: 20px; display: inline-block; padding: 3px 6px;
-                   font-size: 15px; margin: 0px 0px 0px 6px; position: relative; top: -1px; border-radius: 4px;">-20%</span>
-                  <!-- <div class="percent_discount">-30%
+                1.000.000
+                <span class="text-decoration-underline">đ</span>
+              </div>
+            </v-row>
+            <v-row class="ma-0 px-0 align-center">
+              <div class="text-subtitle-2 black--text">
+                1.000.000
+                <span class="text-decoration-underline">đ</span>
+                <span
+                  class=""
+                  style="
+                    color: white;
+                    background: rgb(255, 66, 78);
+                    line-height: 20px;
+                    display: inline-block;
+                    padding: 3px 6px;
+                    font-size: 15px;
+                    margin: 0px 0px 0px 6px;
+                    position: relative;
+                    top: -1px;
+                    border-radius: 4px;
+                  "
+                  >-20%</span
+                >
+                <!-- <div class="percent_discount">-30%
                   </div> -->
-                </div>
-              </v-row>
-            </v-card-text>
-          </v-card>
-        </v-hover>
-      </template>
-    </v-card>
+              </div>
+            </v-row>
+          </v-card-text>
+        </v-card>
+      </v-hover>
+    </template>
+  </v-card>
 </template>
 
 <script>
 export default {
-  name: 'Item',
+  name: "Item",
 
-  data () {
+  data() {
     return {
-      cardState: 'test',
+      cardState: "test",
       rating: [
-        0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-        0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-        0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-        0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-        0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-        0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-        0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-        0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-        0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-        0, 0, 0, 0, 0, 0, 0, 0, 0, 0
+        0,
+        0,
+        0,
+        0,
+        0,
+        0,
+        0,
+        0,
+        0,
+        0,
+        0,
+        0,
+        0,
+        0,
+        0,
+        0,
+        0,
+        0,
+        0,
+        0,
+        0,
+        0,
+        0,
+        0,
+        0,
+        0,
+        0,
+        0,
+        0,
+        0,
+        0,
+        0,
+        0,
+        0,
+        0,
+        0,
+        0,
+        0,
+        0,
+        0,
+        0,
+        0,
+        0,
+        0,
+        0,
+        0,
+        0,
+        0,
+        0,
+        0,
+        0,
+        0,
+        0,
+        0,
+        0,
+        0,
+        0,
+        0,
+        0,
+        0,
+        0,
+        0,
+        0,
+        0,
+        0,
+        0,
+        0,
+        0,
+        0,
+        0,
+        0,
+        0,
+        0,
+        0,
+        0,
+        0,
+        0,
+        0,
+        0,
+        0,
+        0,
+        0,
+        0,
+        0,
+        0,
+        0,
+        0,
+        0,
+        0,
+        0,
+        0,
+        0,
+        0,
+        0,
+        0,
+        0,
+        0,
+        0,
+        0,
+        0,
       ],
-      icons: ['mdi-rewind', 'mdi-play', 'mdi-fast-forward'],
+      icons: ["mdi-rewind", "mdi-play", "mdi-fast-forward"],
       items: [
         {
-          title: 'New Releases',
+          title: "New Releases",
           text: "It's New Release Friday",
-          subtext: 'Newly released songs. Updated daily.',
-          img: 'http://lorempixel.com/output/nightlife-q-c-640-480-5.jpg'
+          subtext: "Newly released songs. Updated daily.",
+          img: "http://lorempixel.com/output/nightlife-q-c-640-480-5.jpg",
         },
         {
-          title: 'Rock',
-          text: 'Greatest Rock Hits',
-          subtext: 'Lose yourself in rock tunes.',
-          img: 'https://images.unsplash.com/photo-1498038432885-c6f3f1b912ee?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2100&q=80'
+          title: "Rock",
+          text: "Greatest Rock Hits",
+          subtext: "Lose yourself in rock tunes.",
+          img:
+            "https://images.unsplash.com/photo-1498038432885-c6f3f1b912ee?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2100&q=80",
         },
         {
-          title: 'Mellow Moods',
-          text: 'Ambient Bass',
-          subtext: 'Chill beats to mellow you out.',
-          img: 'http://lorempixel.com/output/abstract-q-c-640-480-6.jpg'
-        }
+          title: "Mellow Moods",
+          text: "Ambient Bass",
+          subtext: "Chill beats to mellow you out.",
+          img: "http://lorempixel.com/output/abstract-q-c-640-480-6.jpg",
+        },
       ],
-      transparent: 'rgba(255, 255, 255, 0)'
-    }
+      transparent: "rgba(255, 255, 255, 0)",
+    };
   },
-  watch: {
-  },
+  watch: {},
   methods: {
-    getImage () {
+    getImage() {
       // const min = 550
       // const max = 560
-      return 666
+      return 666;
       // return Math.floor(Math.random() * (max - min + 1)) + min
     },
-    detailItem () {
-    }
-  }
-}
+    detailItem() {
+      this.$router.push("/detailitem")
+    },
+  },
+};
 </script>
 
 <style scoped>
@@ -168,7 +277,8 @@ export default {
   /* border-width: 0px; */
 }
 .hover-card {
-  box-shadow: -9px -9px 20px 0 rgba(240, 240, 240, 1.0), 9px 9px 20px 0 rgba(240, 240, 240, 1.0) !important;
+  box-shadow: -9px -9px 20px 0 rgba(240, 240, 240, 1),
+    9px 9px 20px 0 rgba(240, 240, 240, 1) !important;
   z-index: 1;
 
   /* opacity: 1; */
@@ -180,7 +290,7 @@ export default {
   /* border-width: 1px;
   border-style: solid;
   border-color: #c6daff; */
- }
+}
 .hover-img:not(.on-hover) {
   opacity: 0.9;
 }
@@ -196,7 +306,7 @@ export default {
   width: 250px;
   height: 1.2em;
   white-space: nowrap; */
-   display: -webkit-box;
+  display: -webkit-box;
   max-width: 100%;
   margin: 0 auto;
   -webkit-line-clamp: 2;
@@ -213,12 +323,13 @@ export default {
   /* background-image: linear-gradient(to bottom, rgba(245, 245, 245, 0) 0%, transparent 0px); */
 }
 .repeating-gradient {
-  background-image: repeating-linear-gradient(-45deg,
-                      rgba(255,0,0,.25),
-                      rgba(255,0,0,.25) 5px,
-                      rgba(0,0,255,.25) 5px,
-                      rgba(0,0,255,.25) 10px
-                    );
+  background-image: repeating-linear-gradient(
+    -45deg,
+    rgba(255, 0, 0, 0.25),
+    rgba(255, 0, 0, 0.25) 5px,
+    rgba(0, 0, 255, 0.25) 5px,
+    rgba(0, 0, 255, 0.25) 10px
+  );
 }
 .fixedDivContent {
   width: 1000px;
